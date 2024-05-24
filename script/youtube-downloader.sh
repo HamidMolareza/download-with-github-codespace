aa2a97db434a2f8e0061e0fc4d4e92cdbfa47617
Hamid Molareza <molareza@protonmail.com>
2024-05-24 21:13:05 +0000

    fix: youtube downloader

--- script/youtube-downloader.sh
+++ script/youtube-downloader.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-# Check if yt-dlp is installed
-if ! command -v yt-dlp &> /dev/null; then
-    echo "Error: yt-dlp is not installed. Please install it first."
-    exit 1
-fi
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Function to install yt-dlp
+install_yt_dlp() {
+    echo "yt-dlp is not installed. Installing yt-dlp..."
+    sudo apt update
+    sudo apt install -y python3-pip
+    sudo apt install -y python3-brotli
+    sudo pip3 install yt-dlp
+}
 
 # Check if a YouTube link is provided
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <youtube_link>"
+    echo "Usage: $(basename "$0") <youtube_link>"
     exit 1
+fi
+
+# Check if yt-dlp is installed
+if ! command_exists yt-dlp; then
+    install_yt_dlp
 fi
 
 # Get the YouTube link from command-line argument
@@ -20,7 +33,7 @@ echo "Supported formats for downloading:"
 yt-dlp -F "$youtube_link"
 
 # Prompt user to enter the video ID
-read -rp "Enter the video ID you want to download: " video_id
+read -p "Enter the video ID you want to download: " video_id
 
 # Download the video using yt-dlp
-yt-dlp -f "$video_id" "$youtube_link"
+yt-dlp -f $video_id "$youtube_link"
