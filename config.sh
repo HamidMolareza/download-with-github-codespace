086e3df094cb51b5b6078d24c15b25971219dd5b
Hamid Molareza <molareza@protonmail.com>
2024-07-06 17:47:24 +0000

    fix: add scripts sub-folders to path

--- config.sh
+++ config.sh
@@ -3,5 +3,7 @@
 chmod -R +x .
 
 printf "export PATH=\"$PWD/scripts:\$PATH\"" >> ~/.bashrc
+printf "export PATH=\"$PWD/scripts/s3:\$PATH\"" >> ~/.bashrc
+printf "export PATH=\"$PWD/scripts/download:\$PATH\"" >> ~/.bashrc
 
 source ~/.bashrc
