bd5480c14b872c4c30f151f2ebcb1b2a12a0cb4c
Hamid Molareza <molareza@protonmail.com>
2024-07-06 23:45:16 +0000

    feat: add aliases as bashrc configs

--- config.sh
+++ config.sh
@@ -2,10 +2,16 @@
 
 chmod -R +x scripts
 
+printf "\n\n" >> ~/.bashrc
+
 printf "%s\n" "export PATH=\"$PWD/scripts:\$PATH\"" >> ~/.bashrc
 printf "%s\n" "export PATH=\"$PWD/scripts/s3:\$PATH\"" >> ~/.bashrc
 printf "%s\n" "export PATH=\"$PWD/scripts/download:\$PATH\"" >> ~/.bashrc
 printf "%s\n" "export PATH=\"$PWD/scripts/youtube:\$PATH\"" >> ~/.bashrc
+
+printf "\n\n" >> ~/.bashrc
+
+printf "%s\n" ". $PWD/.configs/aliases" >> ~/.bashrc
 
 source "$HOME/.bashrc"
 
