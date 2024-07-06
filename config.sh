92d9fdfcee5b876bb3c77c0bfea9243e7d714059
Hamid Molareza <molareza@protonmail.com>
2024-07-06 18:35:08 +0000

    fix: config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-chmod -R +x .
+chmod -R +x scripts
 
-printf "export PATH=\"$PWD/scripts:\$PATH\"" >> ~/.bashrc
-printf "export PATH=\"$PWD/scripts/s3:\$PATH\"" >> ~/.bashrc
-printf "export PATH=\"$PWD/scripts/download:\$PATH\"" >> ~/.bashrc
+printf "%s\n" "export PATH=\"$PWD/scripts:\$PATH\"" >> ~/.bashrc
+printf "%s\n" "export PATH=\"$PWD/scripts/s3:\$PATH\"" >> ~/.bashrc
+printf "%s\n" "export PATH=\"$PWD/scripts/download:\$PATH\"" >> ~/.bashrc
+printf "%s\n" "export PATH=\"$PWD/scripts/youtube:\$PATH\"" >> ~/.bashrc
 
-source ~/.bashrc
+source "$HOME/.bashrc"
+
+pip install -r "$PWD/scripts/youtube/requirements.txt"
